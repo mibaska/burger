6f22a925dc9c26aaecf5ac65145f912523509ce5
Michael Baska <mikebaska1@gmail.com>
2020-05-09 11:13:06 -0500

    added jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,17 @@
 const mysql = require("mysql");
+var connection;
 
-var source = {
-  localhost: {
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "yourRootPassword",
-    database: "burgers_db"
-  }
-};
+    database: "burgers_db" 
+  });
+}
 
-var connection = mysql.createConnection(source.localhost);
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 module.exports = connection;
